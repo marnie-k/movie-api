848d3f2dd28737b445de735898a82b791dfee077
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-01-03 16:27:43 -0500

    added body parser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getAllMovies, getMovieByTitle } = require('./controllers/movies')
+const bodyParser = require('body-parser')
+const { getAllMovies, getMovieByTitle, addNewMovie } = require('./controllers/movies')
 
 const app = express()
 
 app.get('/movies', getAllMovies)
 app.get('/movies/:search', getMovieByTitle)
+app.post('/movies', bodyParser.json(), addNewMovie)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337....') // eslint-disable-line no-console
