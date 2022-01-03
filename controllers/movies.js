61502ac5c5da76a2b871db5ed6f0d3eb6dca5a19
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-01-03 16:51:46 -0500

    added post route

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,6 +5,7 @@ const movies = require('../movies')
 const getAllMovies = (request, response) => {
   return response.send(movies)
 }
+
 const getMovieByTitle = (request, response) => {
   const { search } = request.params
 
@@ -19,6 +20,18 @@ const getMovieByTitle = (request, response) => {
 }
 
 const addNewMovie = (request, response) => {
+  const {
+    title, directors, releaseDate, rating, runTime, genres
+  } = request.body
+
+  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) return response.status(400).send('These fields cannot be left blank - title, directors, releaseDate, rating, runTime, genres')
+
+  const newMovie = {
+    title, directors, releaseDate, rating, runTime, genres
+}
+movies.push(newMovie)
+
+return response.status(201).send(newMovie)
 
 }
 
